3a093260473c099a7949ec34313891e1f5abba37
HA-SEUNG-JEONG <gktmd652@naver.com>
2024-01-11 23:09:40 +0900

    아이콘 추가

--- site.config.js
+++ site.config.js
@@ -51,7 +51,7 @@ const CONFIG = {
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
-      label: "Comment",
+      label: "#️⃣Comment",
     },
   },
   cusdis: {
